0375019f19a87c6224f529413d47be9da46ef8f9
alex-s-ch <panda.sprechti@gmail.com>
2023-02-03 22:11:24 +0100

    ENH: Return list of node vals

--- invert_binary_tree_226.py
+++ invert_binary_tree_226.py
@@ -9,6 +9,7 @@ class TreeNode:
 class Solution:
     def __init__(self, root):
         self.root = self.invertTree(root=root)
+        self.list_of_vals = []
 
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
@@ -20,11 +21,13 @@ class Solution:
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+
         return root
+
 
     def print_tree(self, root):
         if root:
-            print(root.val)
+            self.list_of_vals.append(root.val)
             self.print_tree(root.left)
             self.print_tree(root.right)
 
@@ -32,11 +35,13 @@ root = TreeNode(4,
                 TreeNode(2, TreeNode(1), TreeNode(3)),
                 TreeNode(7, TreeNode(6), TreeNode(9)))
 
+solution = Solution(root=root)
+solution.print_tree(root=solution.root)
+print(solution.list_of_vals)
+
 # input = [4, 2, 1, 3, 7, 6, 9]
 # output = [4, 7, 9, 6, 2, 3, 1]
+
 #      4                4
 #   2     7          7     2
 # 1  3  6   9      9  6   3  1
-
-solution = Solution(root)
-solution.print_tree(root)
